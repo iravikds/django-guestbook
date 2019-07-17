d092c5501842ec89e3d6fec47551785e9b7eeaf1
RAVIIKMR-LP\Ravi <iraviikmr@gmail.com>
2019-07-17 21:12:41 +0530

    added attrs

--- guest_post/forms.py
+++ guest_post/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('name','body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
